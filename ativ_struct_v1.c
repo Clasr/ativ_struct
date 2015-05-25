20b4b63d382b95d46b60e1a16f837680c493f802
Clarice <clasr7@gmail.com>
2015-05-25 08:53:11 -0300

    adicionada estrutura RGB, bibliotecas, cabeçalho e comentários

---
+++ ativ_struct_v1.c
@@ -0,0 +1,21 @@
+/***************************
+*Atividade sobre Estruturas
+*Clarice Scheibe Ribeiro
+*V1 - 25/05/2015
+***************************/
+
+//bibliotecas
+#include <stdio.h>
+#include <stdlib.h>
+
+//estruturas
+struct ponto2D {
+  float x;
+  float y;
+};
+
+struct CorRGB {
+    int r;
+    int g;
+    int b;
+};
